14123fe08a4cb2243082b59aef43dcfcb28465a0
Jakub Kraus <jakubkraus@proton.me>
2025-08-23 19:47:29 +0200

    Add comprehensive tests for IonChannel module functionality

---
+++ test/runtests.jl
@@ -0,0 +1,11 @@
+using Test
+
+include("../src/IonChannel.jl")
+using .IonChannel
+
+println("Running IonChannel tests...")
+Test.@testset "IonChannel tests" begin
+    include("histogram_tests.jl")
+    include("noise_tests.jl")
+    include("more_tests.jl")
+end
